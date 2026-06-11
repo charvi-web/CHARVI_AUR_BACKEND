4c53b3c8663e82f4498a25411f4607fe6d9147c0
charvi-web <charvisingh077@gmail.com>
2026-06-11 22:40:01 +0530

    Watch history controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from '../models/user.model.js';
 import {uploadOnCloudinary} from '../utils/cloudinary.js';
 import {ApiResponse} from '../utils/ApiResponse.js';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 
 const generateAccessAndRefreshToken = async (userId)=>{
     try{
@@ -385,6 +386,60 @@ const getUserChannelProfile = asyncHandler(
     )
     })
 
+const getWatchHistory = asyncHandler(
+    async(req,res)=>{
+        const user = await User.aggregate(
+            [
+                {
+                    $match:{
+                        _id: new mongoose.Types.ObjectId(req.user._id)
+                    }
+                },
+                {
+                    $lookup:{
+                        from:"videos",
+                        localField:"watchHistory",
+                        foreignField:"_id",
+                        as : "watchHistory",
+                        pipeline:[
+                            {
+                                $lookup:{
+                                    from :"users",
+                                    localField:"owner",
+                                    foreignField:"_id",
+                                    as:"owner",
+                                    pipeline:[
+                                        {
+                                            $projct:{
+                                                fullName :1,
+                                                username:1,
+                                                avatar:1
+                                            }
+                                        }
+                                    ]
+
+                                }
+                            },
+                            {
+                                $addFields:{
+                                    owner:{
+                                        $first:"$owner"
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        )
+        return res.status(200).json(
+            new ApiResponse(200,user[0].watchHistory,"Watch history fetched successfully"
+            )
+        )
+    }
+)
+
+
 export {registerUser,
 loginUser,
     logoutUser,
@@ -392,4 +447,4 @@ loginUser,
    changeCurrentPassword,    getCurrentUser,
     updateAccountDetails,
 updateUserAvatar,updateUserCoverImage,
-getUserChannelProfile}
+getUserChannelProfile,getWatchHistory}
